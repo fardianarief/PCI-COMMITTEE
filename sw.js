d60b4947a0c451137cfade55f99d71b4b0c552ae
Ns. M. Arief Farhiansyah <fardianarief@gmail.com>
2025-12-01 10:15:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10260068
+    "zoneId": 10260092
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
